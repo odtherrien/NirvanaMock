5b75e2d29b2c8382b579be4db5e0e162d0a58de1
odtherrien <olivier@joueur2studios.com>
2018-07-16 19:40:25 -0400

    Initial Commit

---
+++ NirvanaMock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NirvanaMock
+//
+//  Created by TheDuaX on 2018-07-16.
+//  Copyright © 2018 BLIQc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
